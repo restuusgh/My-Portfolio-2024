b2f2426a20e6a875523adadad6ac314dd1ae5f13
restuusgh <restusinggihpasaribu@gmail.com>
2024-12-18 18:41:39 +0700

    finish

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import ParticleBackground from './components/Particle'
+import About from './components/About'
+import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <>
+      <ParticleBackground />
+      <Navbar />
+      <Hero />
+      <About/>
+      <Projects/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-100 py-4 relative'>
+      <div className='container mx-auto px-4 text-center'>
+        <p>&copy; 2024 rohit Singh. All rights reserved</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
